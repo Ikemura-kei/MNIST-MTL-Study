74d44a047f3a1e311fc38b495935d2f7c5e9f80a
IKEMURA-KEI <ikemurakei2001@gmail.com>
2024-05-20 10:19:48 +0200

    added distributed training

--- Toy-MTL/main.py
+++ Toy-MTL/main.py
@@ -2,29 +2,44 @@ import sys
 import os
 
 import torch
+import torch.distributed as dist
+import numpy as np
 from argparse import ArgumentParser
 import json
 import logging
 import time
-from datetime import datetime
+from datetime import datetime as dt
+import datetime
 
 from utils.config_utils import load_cfg, get_model
 
+def set_seed(seed):
+    import random
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
 def main():
+    dist.init_process_group(backend='nccl', init_method='env://', timeout=datetime.timedelta(0, 3600*2))
+
     parser = ArgumentParser()
     parser.add_argument('--cfg_file', type=str, required=True, help='Path to the configuration YAML file.')
     parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'], \
                         help='Mode of execution, if train is chosen, testing will also be performed at the end.')
     parser.add_argument('--ckpt', type=str, default='', help='Path to the checkpoint to load from.')
-    parser.add_argument('--local_rank', type=int, default=0, help='Node rank for distributed training, can be passed from PyTorch distributed launcher.')
+    parser.add_argument('--local-rank', type=int, default=0, help='Node rank for distributed training, can be passed from PyTorch distributed launcher.')
     parser.add_argument('--log_dir', type=str, default='./logs', help='The parent folder to contain all experiment logs.')
 
     # -- load configurations --
     args = parser.parse_args()
     cfg = load_cfg(args)
 
+    # -- set seed --
+    set_seed(cfg.SEED)
+
     # -- create logging directory --
-    data_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    data_time = dt.now().strftime("%Y-%m-%d_%H:%M:%S")
     output_dir = os.path.join(args.log_dir, args.cfg_file.split('/')[-1].replace('.yaml', ''), data_time)
     os.makedirs(output_dir, exist_ok=True)
 
@@ -36,9 +51,16 @@ def main():
     logger.info("=== Configurations ===")
     logger.info(json.dumps(cfg, indent=4))
 
+    logger.info('local rank: %s' %args.local_rank)
+    torch.cuda.set_device(args.local_rank)
+    torch.backends.cudnn.benchmark = True
+
     # -- create model --
-    model = get_model(cfg)
+    model = get_model(cfg).cuda()
     logger.info(model)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank], \
+                                                      output_device=args.local_rank, find_unused_parameters=False)
+    
 
 if __name__ == "__main__":
     main()
--- Toy-MTL/scripts/debug.sh
+++ Toy-MTL/scripts/debug.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
 
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
+            --nproc_per_node=1  \
+            --master_port=$((RANDOM%1000+12000))  \
+            main.py \
+            --cfg_file './cfgs/test.yaml' \
+            --mode train
